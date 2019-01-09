310a4598b59de4f2ee63fa20eca71c282c7089e9
qanah <i.qanah@gmail.com>
2019-01-09 14:56:41 +0200

    fix sender channels

--- src/ChatSDK/Facades/Client.php
+++ src/ChatSDK/Facades/Client.php
@@ -14,9 +14,6 @@ use Exception;
 
 class Client extends Facade
 {
-
-    private static $data = array();
-
     protected static function getFacadeAccessor()
     {
         return 'client';
@@ -28,46 +25,40 @@ class Client extends Facade
             throw new Exception('The app token is required.');
         }
 
-        if(empty(self::$data)) {
+        try {
 
-            try {
+            $client = new HttpClient();
 
-                $client = new HttpClient();
+            $response = $client->request('GET', 'service/sdk/info', [
+                'headers' => [
+                    'Accept-Token' => Config::get('app_token'),
+                ]
+            ]);
 
-                $response = $client->request('GET', 'service/sdk/info', [
-                    'headers' => [
-                        'Accept-Token' => Config::get('app_token'),
-                    ]
-                ]);
-
-                if($response->getStatusCode() != 200) {
-                    throw new Exception('The remote endpoint could not be called, or the response it returned was invalid.');
-                }
-
-                $contents = json_decode($response->getBody()->getContents(), true);
-
-                if(!is_array($contents)) {
-                    $contents = array();
-                }
-
-                $contents['host'] = Config::get('host');
-                $contents['port'] = 1883;
-
-                if(isset($contents['id'])) {
-                    $contents['client_id'] = uniqid("service_{$contents['id']}_");
-                    $contents['receive_client_id'] = "service_{$contents['id']}_receive";
-                    $contents['topic_prefix'] = "grp/srv-{$contents['id']}";
-                }
-
-                self::$data = $contents;
-
-            } catch (Exception $e) {
-                throw $e;
+            if($response->getStatusCode() != 200) {
+                throw new Exception('The remote endpoint could not be called, or the response it returned was invalid.');
             }
 
-        }
+            $contents = json_decode($response->getBody()->getContents(), true);
 
-        static::swap(new Repository(self::$data));
+            if(!is_array($contents)) {
+                $contents = array();
+            }
+
+            $contents['host'] = Config::get('host');
+            $contents['port'] = 1883;
+
+            if(isset($contents['id'])) {
+                $contents['client_id'] = uniqid("service_{$contents['id']}_");
+                $contents['receive_client_id'] = "service_{$contents['id']}_receive";
+                $contents['topic_prefix'] = "grp/srv-{$contents['id']}";
+            }
+
+            static::swap(new Repository($contents));
+
+        } catch (Exception $e) {
+            throw $e;
+        }
 
     }
 }
