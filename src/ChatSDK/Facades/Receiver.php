f9a95f5301811b1bc4667e5b2842e621775c1026
qanah <i.qanah@gmail.com>
2019-01-09 14:56:41 +0200

    fix sender channels

--- src/ChatSDK/Facades/Receiver.php
+++ src/ChatSDK/Facades/Receiver.php
@@ -14,9 +14,6 @@ use Exception;
 
 class Receiver extends Facade
 {
-
-    private static $data = array();
-
     protected static function getFacadeAccessor()
     {
         return 'receiver';
@@ -28,42 +25,36 @@ class Receiver extends Facade
             throw new Exception('The app token is required.');
         }
 
-        if(empty(self::$data[$topic][$account_sender_id])) {
+        try {
 
-            try {
+            $client = new HttpClient();
 
-                $client = new HttpClient();
+            $response = $client->request('POST', "service/sdk/receiver", [
+                'headers' => [
+                    'Accept' => 'application/json',
+                    'Accept-Token' => Config::get('app_token'),
+                ],
+                'form_params' => [
+                    'topic' => $topic,
+                    'account_sender_id' => $account_sender_id,
+                ]
+            ]);
 
-                $response = $client->request('POST', "service/sdk/receiver", [
-                    'headers' => [
-                        'Accept' => 'application/json',
-                        'Accept-Token' => Config::get('app_token'),
-                    ],
-                    'form_params' => [
-                        'topic' => $topic,
-                        'account_sender_id' => $account_sender_id,
-                    ]
-                ]);
-
-                if($response->getStatusCode() != 200) {
-                    throw new Exception('The remote endpoint could not be called, or the response it returned was invalid.');
-                }
-
-                $contents = json_decode($response->getBody()->getContents(), true);
-
-                if(!is_array($contents)) {
-                    $contents = array();
-                }
-
-                self::$data[$topic][$account_sender_id] = $contents;
-
-            } catch (Exception $e) {
-                throw $e;
+            if($response->getStatusCode() != 200) {
+                throw new Exception('The remote endpoint could not be called, or the response it returned was invalid.');
             }
 
-        }
+            $contents = json_decode($response->getBody()->getContents(), true);
 
-        static::swap(new Repository(self::$data[$topic][$account_sender_id]));
+            if(!is_array($contents)) {
+                $contents = array();
+            }
+
+            static::swap(new Repository($contents));
+
+        } catch (Exception $e) {
+            throw $e;
+        }
 
     }
 }
--- src/ChatSDK/Facades/Sender.php
+++ src/ChatSDK/Facades/Sender.php
@@ -14,8 +14,6 @@ use Exception;
 
 class Sender extends Facade
 {
-    private static $data = array();
-
     protected static function getFacadeAccessor()
     {
         return 'sender';
@@ -27,42 +25,36 @@ class Sender extends Facade
             throw new Exception('The app token is required.');
         }
 
-        if(empty(self::$data[$topic][$phone])) {
+        try {
 
-            try {
+            $client = new HttpClient();
 
-                $client = new HttpClient();
+            $response = $client->request('POST', "service/sdk/sender", [
+                'headers' => [
+                    'Accept' => 'application/json',
+                    'Accept-Token' => Config::get('app_token'),
+                ],
+                'form_params' => [
+                    'topic' => $topic,
+                    'phone' => $phone,
+                ]
+            ]);
 
-                $response = $client->request('POST', "service/sdk/sender", [
-                    'headers' => [
-                        'Accept' => 'application/json',
-                        'Accept-Token' => Config::get('app_token'),
-                    ],
-                    'form_params' => [
-                        'topic' => $topic,
-                        'phone' => $phone,
-                    ]
-                ]);
-
-                if($response->getStatusCode() != 200) {
-                    throw new Exception('The remote endpoint could not be called, or the response it returned was invalid.');
-                }
-
-                $contents = json_decode($response->getBody()->getContents(), true);
-
-                if(!is_array($contents)) {
-                    $contents = array();
-                }
-
-                self::$data[$topic][$phone] = $contents;
-
-            } catch (Exception $e) {
-                throw $e;
+            if($response->getStatusCode() != 200) {
+                throw new Exception('The remote endpoint could not be called, or the response it returned was invalid.');
             }
 
-        }
+            $contents = json_decode($response->getBody()->getContents(), true);
 
-        static::swap(new Repository(self::$data[$topic][$phone]));
+            if(!is_array($contents)) {
+                $contents = array();
+            }
+
+            static::swap(new Repository($contents));
+
+        } catch (Exception $e) {
+            throw $e;
+        }
 
     }
 }
