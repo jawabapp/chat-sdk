8202df2aa3c5ec6ef15b37aa353f3c8ec7fa203b
qanah <i.qanah@gmail.com>
2019-02-07 15:59:21 +0200

    add Subscribe channel

--- tests/deep_links.php
+++ tests/deep_links.php
@@ -12,13 +12,13 @@ use ChatSDK\Facades\Config;
 use ChatSDK\Channels\DeepLinksChannel;
 
 Config::make([
-    'app_token' => 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjIifQ.eyJpc3MiOiJodHRwOlwvXC9jaGF0Lmphd2FiLmFwcCIsImF1ZCI6InN0YWdpbmcgamF3YWJrb20iLCJqdGkiOiIyIiwiaWF0IjoxNTQ2NDM2MzE3LCJuYmYiOjE1NDY0MzYzMTcsImV4cCI6MTU3NzgzNjgwMCwidWlkIjoyfQ.MVP3St47eXHncn_BHTAutNmPRkpi5hzio2ciMYlMO262xJH7DsMHLOvED7P8zd2WASehiPaVy7Sd1qkfmOAMYu3-d_Ok2Hpb2z08CMoBiLmNbQ6MfJMV5sP6FyI3U1Sr9wrdN6Vhu785RWQNL3v55N4EDiS_mG7030GHZ48t92CoDQ6a-lchcYyFNwn208_lCAgj30pk7SZIYOJMoTwglIsfE5R1Isb0gfzr30NxHUs0QNBeR8xYyRj0ljZJDuTuuWNzcaXUr0SvuyPixD1_CEeoU-gwsgLp5hbq_GYUN10sqgEct82kQqUYPD48RN-BBAM-VIsygrKvXysWuikKJMcV86xkPieRrtt-so2M2aKY7YnxZAJotFW8fNowcYyjCN47G9Tj4lRxscDTzxC4quf_n8D6vQ9RYEQsFyVW-GoYkm3XqYlhliJQROoR9JVMPhpJfWmtY8yTxUQVQYCsFAS47Lob_rGItNdlJ4YAlxXipHM8xvbgHGYDc5UKNMEpdVsG76Rh89RrZJ6TN8lCqDaGaVzJ40dCmE6ZRggRHdGpEV4le6hFNWpKTL8U98WfOyKuaEFMWgG0EMTmlDP-3pxrG3hDc1N64IqvkCD8ry6SYCRH4s2e6slX0XUnHY0Xy2X9plE-RBWnWa52Jh3pSV_pKihA2I965wKC6vsvAhA'
+    'app_token' => 'token.token.token.token'
 ]);
 
 $topic = new \ChatSDK\Support\Topic();
 
 //if qoustion id exist in table
-$topic->setTopic('grp/srv-2/45255077_5c1a63f832fca');
+$topic->setTopic('grp/srv-1/45255077_5c1a63f832fca');
 //$topic->generateTopic(45255077);
 
 $auth_subscription_link = DeepLinksChannel::generate_shortcut_subscription_link(
