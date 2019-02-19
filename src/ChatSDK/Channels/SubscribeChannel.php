03125aa4936a4841d9676c08acedc89839440e3c
qanah <i.qanah@gmail.com>
2019-02-19 20:17:05 +0200

    add Subscribe channel

--- src/ChatSDK/Channels/SubscribeChannel.php
+++ src/ChatSDK/Channels/SubscribeChannel.php
@@ -37,7 +37,7 @@ class SubscribeChannel
             'form_params' => [
                 'user_phone' => $params['user_phone'],
                 'user_nickname' => $params['user_nickname'],
-                'user_topics' => $params['topics'],
+                'user_topics' => $params['user_topics'],
                 'os' => $params['os'],
                 'device_info' => $params['device_info'],
                 'purchased_at' => $params['purchased_at'],
