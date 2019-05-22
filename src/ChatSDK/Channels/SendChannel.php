968148a1c3ce00daaa19d02e27c6e50dcb1f8a91
Ibraheem Qanah <i.qanah@gmail.com>
2019-05-22 15:25:37 +0300

    add subscription type

--- src/ChatSDK/Channels/SendChannel.php
+++ src/ChatSDK/Channels/SendChannel.php
@@ -28,8 +28,8 @@ class SendChannel
             throw new Exception('invalid client id');
         }
 
-        if(!in_array($content_type, ['text', 'image'])){
-            throw new Exception('The content type must be (text,image)');
+        if(!in_array($content_type, ['text', 'image', 'subscription'])){
+            throw new Exception('The content type must be (text,image,subscription)');
         }
 
         if (strpos($topic, Client::get('topic_prefix') . '/') !== 0) {
