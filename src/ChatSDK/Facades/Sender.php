479420ed32a010944f455de03243a85b8d879ec6
Ibraheem Qanah <i.qanah@gmail.com>
2019-06-10 03:37:53 +0300

    add subscription type

--- src/ChatSDK/Facades/Sender.php
+++ src/ChatSDK/Facades/Sender.php
@@ -14,6 +14,8 @@ use Exception;
 
 class Sender extends Facade
 {
+    private static $cache = array();
+
     protected static function getFacadeAccessor()
     {
         return 'sender';
@@ -27,27 +29,33 @@ class Sender extends Facade
 
         try {
 
-            $client = new HttpClient();
+            if(!empty(self::$cache[$topic][$phone])) {
+                $contents = self::$cache[$topic][$phone];
+            } else {
+                $client = new HttpClient();
 
-            $response = $client->request('POST', "sdk/sender", [
-                'headers' => [
-                    'Accept' => 'application/json',
-                    'Accept-Token' => Config::get('app_token'),
-                ],
-                'form_params' => [
-                    'topic' => $topic,
-                    'phone' => $phone,
-                ]
-            ]);
+                $response = $client->request('POST', "sdk/sender", [
+                    'headers' => [
+                        'Accept' => 'application/json',
+                        'Accept-Token' => Config::get('app_token'),
+                    ],
+                    'form_params' => [
+                        'topic' => $topic,
+                        'phone' => $phone,
+                    ]
+                ]);
 
-            if($response->getStatusCode() != 200) {
-                throw new Exception('The remote endpoint could not be called, or the response it returned was invalid.');
-            }
+                if($response->getStatusCode() != 200) {
+                    throw new Exception('The remote endpoint could not be called, or the response it returned was invalid.');
+                }
 
-            $contents = json_decode($response->getBody()->getContents(), true);
+                $contents = json_decode($response->getBody()->getContents(), true);
 
-            if(!is_array($contents)) {
-                $contents = array();
+                if(!is_array($contents)) {
+                    $contents = array();
+                }
+
+                self::$cache[$topic][$phone] = $contents;
             }
 
             static::swap(new Repository($contents));
@@ -56,5 +64,9 @@ class Sender extends Facade
             throw $e;
         }
 
+    }
+
+    public static function clearCache() {
+        self::$cache = array();
     }
 }
